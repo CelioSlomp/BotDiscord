fed298149dd1351dfd87f255bc953e431ec1a701
Celio Slomp <slompcelio132@gmail.com>
2021-02-17 14:53:38 -0300

    add: First commit

---
+++ Main.py
@@ -0,0 +1,48 @@
+"""Programa para um bot de discord
+
+Autor:
+@Celio Ludwig Slomp
+Instagram: celio.ls
+Twitter: celio_ls_
+Github: https://github.com/CelioSlomp
+"""
+
+import discord
+from discord.ext import commands
+import random
+
+# Foi criado o objeto 'cliente' para setar os eventos e comandos 
+# usados posteriormente.
+cliente = commands.Bot(command_prefix="&")
+
+# Quando rodar o programa, o bot estará funcionando a partir 
+# do momento que printar 'Bot está online.' no cmd/terminal.
+@cliente.event
+async def on_ready():
+    print("Bot está online.")
+
+# @Placeholder, serve apenas para teste de comandos.
+@cliente.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+# @Placeholder, serve apenas para teste de comandos.
+@cliente.command()
+async def pong(ctx):
+    await ctx.send("Não cara, deu, tá chatão já")
+
+# É para mostrar todos os comandos que o bot possui. Por enquanto
+# contando com os de placeholder, possuem 4 comandos.
+@cliente.command()
+async def commands(ctx):
+    await ctx.send("Por enquanto nós temos os seguintes comandos: \n\
+&ping - Irá ter uma resposta legal - \n\
+&pong - Irá ter outra resposta legal -")
+
+# Dá clear no chat, comando muito útil porém perigoso.
+@cliente.command()
+async def clear(ctx, quantidade=5): # Adicionar uma certa restrição para
+    await ctx.channel.purge(limit=quantidade) # quem pode utilizar isso.
+
+# Aqui irá 'linkar' o código com o bot do Discord.
+cliente.run("ODEwOTU3NTg4MTg3MzE2MjI0.YCrNGw.XWsbDh2V-VNlhbZVg6qIMORPrCQ")
---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Olá caro usuário.
+
+- A finalidade desse bot é apenas a de aprendizado, então fique a vontade de modificar
+o mesmo, mas não copie, ok?
+
+- The objective about this bot is only for learn. So, be free to modify this, but
+don't copy, ok?
